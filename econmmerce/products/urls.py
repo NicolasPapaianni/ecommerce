32e71842bd1d50f75082956d0bb63427dd74447f
Nicolas Papaianni <papaianninicolas@gmail.com>
2022-08-05 14:17:50 +0200

    forms

--- econmmerce/products/urls.py
+++ econmmerce/products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from products.views import list_products, create_product
+from products.views import list_products, create_product, primer_formulario
 
 urlpatterns = [
     path('list-products/', list_products, name = 'list_products'),
     path('create-product/', create_product, name = 'create_product'),
+    path('primer_formulario/', primer_formulario, name= 'primer_formulario')
+    
 ]
--- econmmerce/products/views.py
+++ econmmerce/products/views.py
@@ -1,15 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from products.models import Products
+from products.forms import Formulario_productos
 
 # Create your views here.
 
 def create_product(request):
-    new_product = Products.objects.create(name = 'Crush 500 ml', price = 150, stock= 30)
-    context = {
-        'new_product' : new_product
-    }
-    return render( request, 'products/new_product.html', context=context)
+
+
+    if request.method == 'POST':
+        pass
+    #     new_product = Products.objects.create(name = 'Crush 500 ml', price = 150, stock= 30)
+    #     context = {
+    #         'new_product' : new_product
+    # }
+
+    elif request.method == 'GET':
+        form = Formulario_productos
+        context = {'form': form}
+        return render( request, 'products/new_product.html', context=context)
 
 def list_products(request):
     products = Products.objects.all()
@@ -19,4 +28,8 @@ def list_products(request):
     return render(request, 'products/products_list.html', context=context)
 
 
-    
+def primer_formulario(request):
+    print(request.method)
+    if request.method == 'POST':
+        print(request.POST)
+    return render(request, 'products/primer_formulario.html', context={})   
